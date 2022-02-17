d875758f1e4d5993e91df3db4a778ecd435cb20e
Marcos Andre <marcosdev.me@gmail.com>
2022-02-17 14:04:07 -0300

    feat: Add provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/Auth/AuthProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
